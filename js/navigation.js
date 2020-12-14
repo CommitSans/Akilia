36e294a01bfceb4ee5109c798004df4b96f972de
CodingCarlos <hola@codingcarlos.com>
2020-12-14 18:58:01 +0100

    Fill navbar on scroll

--- js/navigation.js
+++ js/navigation.js
@@ -1,56 +1,17 @@
 $(document).ready(function () {
 
-	var threshold = 200;	// Pixels before menu activation	
+	var threshold = 200;	// Pixels before topbar	color
+	var inactiveClass = 'bg-transparent';	// Active navbar class
 
-	var body = $('body');
-
-	var home = $('#home');
-	var manifesto = $('#manifesto');
-	var portfolio = $('#portfolio');
-	var about = $('#about');
-
-	var homeLink = $('nav a[href="#home"]');
-	var manifestoLink = $('nav a[href="#manifesto"]');
-	var portfolioLink = $('nav a[href="#portfolio"]');
-	var aboutLink = $('nav a[href="#about"]');
-
-	$('nav a').on('click', function(event) {
-		activate($(this));
-	});
+	var navbar = $('nav');
 
 	$(document).on('scroll', function(event) {
-		if (body.hasClass('scrolling')) {
-			return false;
-		}
-		
 		var scroll = $('html').scrollTop();
 
-    	if (scroll <= manifesto.offset().top - threshold) {
-    		activate(homeLink);
-    	} else if(manifesto.offset().top - threshold <= scroll
-    		&& scroll < (manifesto.offset().top + manifesto.height() - threshold)) {
-    		activate(manifestoLink);
-    	} else if(portfolio.offset().top - threshold <= scroll
-    		&& scroll < (portfolio.offset().top + portfolio.height() - threshold)) {
-    		activate(portfolioLink);
-    	} else if(about.offset().top - threshold <= scroll
-    		&& scroll < (about.offset().top + about.height() - threshold)) {
-    		activate(aboutLink);
+    	if (scroll >= threshold) {
+    		navbar.removeClass(inactiveClass);
+    	} else {
+    		navbar.addClass(inactiveClass);
     	}
-	})
-
-	var activeClass = 'active';
-
-	function deactivateAll() {
-		return $('.' + activeClass).removeClass(activeClass);
-	}
-
-	function activate(elem) {
-		if(elem.hasClass(activeClass)) {
-			return true;
-		}
-
-		deactivateAll();
-		return elem.addClass(activeClass);
-	}
+	});
 });
